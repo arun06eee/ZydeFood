6590abd43ff9b1898e4e8061c6ac6db4371b1a90
dhineshkumar256 <rajdhinesh256@gmail.com>
2016-11-09 19:29:44 +0530

    remove all session

--- system/tastyigniter/logs/logs.php
+++ system/tastyigniter/logs/logs.php
@@ -1,3 +1,4 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 ERROR - 2016-11-09 13:52:51 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp\www\ZydeFood\admin\views\themes\tastyigniter-blue\orders.php 15
+ERROR - 2016-11-09 13:58:34 --> Severity: Warning --> Invalid argument supplied for foreach() C:\wamp\www\ZydeFood\admin\views\themes\tastyigniter-blue\orders.php 15
